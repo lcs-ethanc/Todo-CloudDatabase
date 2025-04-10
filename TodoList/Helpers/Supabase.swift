c392aad88e65a61b451438822dd91c71a674db1b
Ethan Chen <echen@lcs.on.ca>
2025-04-09 20:12:39 -0400

    Added a helper file to identify our app to the Supabase servers.

---
+++ TodoList/Helpers/Supabase.swift
@@ -0,0 +1,15 @@
+//
+//  Supabase.swift
+//  TodoList
+//
+//  Created by Pak Ching Ethan Chen on 2025-04-09.
+//
+
+import Foundation
+import Supabase
+ 
+let supabase = SupabaseClient(
+  supabaseURL: URL(string: "https://gbigmkyhbmjhgpenlswn.supabase.co")!,
+  supabaseKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdiaWdta3loYm1qaGdwZW5sc3duIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDM5ODMyOTEsImV4cCI6MjA1OTU1OTI5MX0.g5weyOetZQfRPdb7v8_S8UHl4UzuijcRvm-65V_lZYs"
+)
+ 
