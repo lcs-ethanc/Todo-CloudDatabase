56ac6014965efa4f3501bb5a144851705486816e
Ethan Chen <echen@lcs.on.ca>
2025-04-11 08:28:45 -0400

    Added an update function and made view model accessible to subviews so that changes to to-do items are persisted.

--- TodoList/Views/ItemView.swift
+++ TodoList/Views/ItemView.swift
@@ -11,15 +11,22 @@ struct ItemView: View {
     
     @Binding var currentItem: TodoItem
     
+    //Recieve reference to viewmodel from parent view
+    @Bindable var viewModel: TodoListViewModel
+    
     var body: some View {
         Label(
             title: {
                 TextField("", text: $currentItem.title, axis: .vertical)
+                    .onSubmit {
+                        viewModel.update(todo: currentItem)
+                    }
             }, icon: {
                 Image(systemName: currentItem.done == true ? "checkmark.circle" : "circle")
                     // Tap to mark as done
                     .onTapGesture {
                         currentItem.done.toggle()
+                        viewModel.update(todo: currentItem)
                     }
                 
             }
@@ -28,8 +35,11 @@ struct ItemView: View {
 }
 
 #Preview {
-    List {
-        ItemView(currentItem: .constant(firstItem))
-        ItemView(currentItem: .constant(secondItem))
+    
+    @State var previewsViewModel = TodoListViewModel()
+    
+        return List {
+            ItemView(currentItem: .constant(firstItem), viewModel: previewsViewModel)
+            ItemView(currentItem: .constant(secondItem), viewModel: previewsViewModel)
     }
 }
